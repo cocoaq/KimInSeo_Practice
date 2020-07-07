9c31b7dbf54147239ea174861753523bef05cf38
인서 <@DESKTOP-J836M3E>
2020-07-08 02:08:57 +0900

    다햇다

---
+++ src/p_7/test/model/Sandwich.java
@@ -0,0 +1,12 @@
+package p_7.test.model;
+
+public abstract class Sandwich {
+
+	public abstract String sand();
+	public void breadUp() {
+		System.out.println("(    )");
+	}
+	public void breadDown() {
+		System.out.println("======");
+	}
+}
---
+++ src/p_7/test/model/TunaSand.java
@@ -0,0 +1,10 @@
+package p_7.test.model;
+
+public class TunaSand extends Sandwich{
+
+	@Override
+	public String sand() {
+		return "######";
+	}
+
+}
